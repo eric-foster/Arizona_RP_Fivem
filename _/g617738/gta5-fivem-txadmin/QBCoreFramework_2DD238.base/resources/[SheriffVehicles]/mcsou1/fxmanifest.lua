40c1b3297f29f3a8262aa9a63c381be2ae7b9023
Eric Foster <etfoster.1001@yahoo.com>
2024-05-04 10:07:22 -0400

    Added Files

---
+++ _/g617738/gta5-fivem-txadmin/QBCoreFramework_2DD238.base/resources/[SheriffVehicles]/mcsou1/fxmanifest.lua
@@ -0,0 +1,41 @@
+fx_version 'adamant'
+games { 'gta5' }
+
+
+files {
+	'data/**/*.meta',
+	'data/**/*.dat10',
+	'data/**/*.dat10.nametable',
+	'data/**/*.dat10.rel',
+	'data/**/*.dat151',
+	'data/**/*.dat151.nametable',
+	'data/**/*.dat151.rel',
+	'data/**/*.dat15',
+	'data/**/*.dat15.nametable',
+	'data/**/*.dat15.rel',
+	'data/**/*.dat54',
+	'data/**/*.dat54.nametable',
+	'data/**/*.dat54.rel',
+	'data/**/*.dat4',
+	'data/**/*.dat4.nametable',
+	'data/**/*.dat4.rel',
+	'data/**/*.awc',
+	'data/**/clip_sets.xml'
+}
+
+data_file 'VEHICLE_LAYOUTS_FILE' 'data/**/vehiclelayouts.meta'
+data_file 'HANDLING_FILE' 'data/**/handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'data/**/vehicles.meta'
+data_file 'CARCOLS_FILE' 'data/**/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'data/**/carvariations.meta'
+data_file 'CONTENTUNLOCKS_METADATA_FILE' 'data/**/contentunlocks.meta'
+data_file 'DLCTEXT_METADATA_FILE'  'data/**/dlctext.meta'
+
+data_file 'AUDIO_GAMEDATA' 'data/vehicles-civ-casino/audio/dlcvinewood_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'data/vehicles-civ-casino/audio/dlcvinewood_sounds.dat'
+data_file 'AUDIO_DYNAMIXDATA' 'data/vehicles-civ-casino/audio/dlcvinewood_mix.dat'
+data_file 'AUDIO_SYNTHDATA' 'data/vehicles-civ-casino/audio/dlcvinewood_amp.dat'
+data_file 'AUDIO_SPEECHDATA' 'data/vehicles-civ-casino/audio/dlcvinewood_speech.dat'
+data_file 'AUDIO_WAVEPACK' 'data/vehicles-civ-casino/audio/sfx/dlc_vinewood'
+
+data_file 'CLIP_SETS_FILE' 'data/**/clip_sets.xml'
