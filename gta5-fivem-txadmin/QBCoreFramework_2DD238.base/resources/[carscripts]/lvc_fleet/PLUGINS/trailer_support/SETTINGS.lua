a2975e0566ec5a311237a720730e60c890123fe4
Eric Foster <etfoster.1001@yahoo.com>
2024-05-05 01:13:32 -0400

    File Restructuring

--- gta5-fivem-txadmin/QBCoreFramework_2DD238.base/resources/[carscripts]/lvc_fleet/PLUGINS/trailer_support/SETTINGS.lua
+++
@@ -1,7 +0,0 @@
---------------------TRAILER SUPPORT SETTINGS---------------------
-trailer_masterswitch = false
---	Determines if trailer_support plugin can be activated.
-
---[[ Documentation / Wiki: https://docs.luxartengineering.com/shared-plugins/trailer-support ]]
-
-TRAILERS = { }
--- gta5-fivem-txadmin/QBCoreFramework_2DD238.base/resources/[sonoranscripts]/sonoran-trafficcam_helper/fxmanifest.lua
+++
@@ -1,8 +0,0 @@
-fx_version 'bodacious'
-games {'gta5'}
-
-lua54 'yes'
-server_only 'yes'
-server_scripts {'s.lua', 'utility.lua'}
-
-dependency '/assetpacks'
